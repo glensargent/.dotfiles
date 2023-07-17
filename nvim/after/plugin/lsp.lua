2d087941104c9567cd08850dd85d225ca40307b1
Glen Sargent <glensarg@hotmail.co.uk>
2023-07-17 11:19:46 +0100

    chore: lsp keybinds

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -17,4 +17,18 @@ vim.diagnostic.config({
   virtual_text = true
 })
 
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  end,
+})
