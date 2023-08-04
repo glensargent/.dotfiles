01d05226bb520d005866d9d41155e35318b92307
Glen Sargent <glensarg@hotmail.co.uk>
2023-08-04 11:38:33 +0100

    chore: add gi

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -30,6 +30,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
