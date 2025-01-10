77717738c95d8d583ba235cfbeecd8d27aa799ec
Glen Sargent <glensarg@hotmail.co.uk>
2025-01-10 17:06:59 +0000

    update: bind telescope marks

--- nvim/lua/glen/keymap.lua
+++ nvim/lua/glen/keymap.lua
@@ -45,6 +45,9 @@ keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string u
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
+-- telescope marks
+keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>") -- list all marks
+
 -- telescope git commands (not on youtube nvim video)
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
