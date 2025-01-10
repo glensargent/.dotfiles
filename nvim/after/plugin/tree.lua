62fbf2b8da1cddfddb36d5df5b885a4a2f1a8fd5
Glen Sargent <glensarg@hotmail.co.uk>
2025-01-10 13:32:17 +0000

    move tree to centre of screen and hide by default

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,5 +1,3 @@
--- examples for your init.lua
-
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -7,8 +5,44 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+local HEIGHT_RATIO = 0.8 
+local WIDTH_RATIO = 0.5  
+
 -- -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    disable_netrw = true,
+    hijack_netrw = true,
+    respect_buf_cwd = true,
+    sync_root_with_cwd = true,
+    view = {
+        relativenumber = true,
+        float = {
+            enable = true,
+            open_win_config = function()
+                local screen_w = vim.opt.columns:get()
+                local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                local window_w = screen_w * WIDTH_RATIO
+                local window_h = screen_h * HEIGHT_RATIO
+                local window_w_int = math.floor(window_w)
+                local window_h_int = math.floor(window_h)
+                local center_x = (screen_w - window_w) / 2
+                local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                    - vim.opt.cmdheight:get()
+                return {
+                    border = "rounded",
+                    relative = "editor",
+                    row = center_y,
+                    col = center_x,
+                    width = window_w_int,
+                    height = window_h_int,
+                }
+            end,
+        },
+        width = function()
+            return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+        end,
+    },
+})
 
 -- OR setup with some options
 -- require("nvim-tree").setup({
