87ae5aa1556d19dd12e0c39234c9abae8df97cc9
Glen Sargent <glensarg@hotmail.co.uk>
2025-03-29 11:37:52 +0000

    feat: replace cmp with blink

--- nvim/lua/glen/lazy/lsp.lua
+++ nvim/lua/glen/lazy/lsp.lua
@@ -5,16 +5,16 @@ return {
       -- LSP Support
       {'neovim/nvim-lspconfig'},    -- Required
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
-      {'hrsh7th/cmp-cmdline'},      -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+    --   {'hrsh7th/nvim-cmp'},         -- Required
+    --   {'hrsh7th/cmp-nvim-lsp'},     -- Required
+    --   {'hrsh7th/cmp-buffer'},       -- Optional
+    --   {'hrsh7th/cmp-path'},         -- Optional
+    --   {'hrsh7th/cmp-nvim-lua'},     -- Optional
+    --   {'hrsh7th/cmp-cmdline'},      -- Optional
+    --   {'saadparwaiz1/cmp_luasnip'}, -- Optional
       -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'} -- Optional
+    --   {'L3MON4D3/LuaSnip'},             -- Required
+    --   {'rafamadriz/friendly-snippets'} -- Optional
   },
   config = function()
     -- lsp zero
